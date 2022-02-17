030d435aa747f0f41b5604a3e867daa8d4b7d87f
Chris Martinez <chmartinez@lumeris.com>
2022-02-17 10:10:55 -0600

    finishing spirograph with colors

--- walk_polygon.py
+++ walk_polygon.py
@@ -5,12 +5,14 @@ from turtle import *
 def polygon(self):
     def draw_shape(num_sides):
         """Accepts int of degrees for angle to draw shape"""
-        begin_fill()
+        f_color = random_color()
+        self.fillcolor(f_color)
+        self.begin_fill()
         angle = (360 / num_sides)
         for _ in range(num_sides):
             self.forward(100)
             self.right(angle)
-        end_fill()
+        self.end_fill()
     
     for shape_side_n in range(3, 11):
         # pen_color()
--- walk_spirograph.py
+++ walk_spirograph.py
@@ -1,10 +1,17 @@
 from turtle import *
 from colors import random_color
 
+
 def spirograph(self):
+    """Accepts turtle object as argument """
+    self.speed('fastest')
     for _ in range(300):
         t_color = random_color()
+        f_color = random_color()
         self.color(t_color)
+        self.fillcolor(f_color)
+        self.begin_fill()
         self.circle(100)
+        self.end_fill()
         new_heading = self.heading() + 5
         self.setheading(new_heading)
